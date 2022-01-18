9fe04bb6b5c116231e42b86ee71fa198d745e113
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-18 15:14:12 -0500

    fixed npm error. starts successfully and connects to db

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { Sequelize } = require('sequelize/dist');
+// const { Sequelize } = require('sequelize/dist');
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
+
 
 const sess = {
     secret: 'secretecat',
@@ -23,7 +26,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(session(sess));
 
