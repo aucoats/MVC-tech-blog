8ea12a09fc15824bd1ffd1dbfcf3feeb9244f391
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-20 15:23:57 -0500

    login/logout comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Comment, Vote } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
-// get all users
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
